23cdfd0a080eab213242e0f2afc608ca9c92da34
miracle <q00yuriy@mail.ru>
2023-03-02 13:41:20 +0300

    task 1_0

---
+++ lesson_1/1_0/Program.cs
@@ -0,0 +1,7 @@
+﻿// 0. Напишите программу, которая на вход принимает число 
+// выдает его кввадрат (число умноженное само на себя)
+
+// string s_a = Console.ReadLine()!;
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(a * a);
