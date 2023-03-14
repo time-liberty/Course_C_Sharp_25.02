849018146b6711e42f8235aa95dea38f1e5a8326
miracle <q00yuriy@mail.ru>
2023-03-14 23:39:21 +0300

    Task 3_2

---
+++ lesson_3/3_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая
+//принимает на вход координаты двух точек и
+//находит расстояние между ними в 2D
+//пространстве.
+
+double example(double x1, double y1, double x2, double y2 )
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2)+Math.Pow( y2 - y1, 2 ));
+
+}
+Console.WriteLine(example(3,6,2,1));
