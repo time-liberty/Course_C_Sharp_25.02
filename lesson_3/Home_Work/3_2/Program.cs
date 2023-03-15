cd5a1a1a253f053a139a673bf31cedb6aa60166a
miracle <q00yuriy@mail.ru>
2023-03-15 21:35:34 +0300

    HW Task 3_2

--- lesson_3/Home_Work/3_2/Program.cs
+++ lesson_3/Home_Work/3_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+
+double example(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2)+Math.Pow( y2 - y1, 2 ) + Math.Pow( z2 - z1 , 2));
+
+}
+Console.WriteLine(example(3,6,8,2,1,-7));
