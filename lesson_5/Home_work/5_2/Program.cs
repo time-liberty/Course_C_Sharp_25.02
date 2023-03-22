b87a2e0c564061bd94989d87fe38c177d74c42e4
miracle <q00yuriy@mail.ru>
2023-03-22 21:47:05 +0300

    HW Task 5_2

---
+++ lesson_5/Home_work/5_2/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size,int from, int to)
+{
+    int[] arr = new int[size];
+
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from,to);
+    }
+    return arr;
+}
+int SumNegPos(int[] arr)
+{
+    int sum = 0;
+    for (int i =0; i < arr.Length; i++)
+    {
+        if ( i % 2 == 1)
+        {
+            sum = sum + arr[i];
+        }
+    }
+    Console.WriteLine(sum);
+    return sum;
+}
+
+Console.WriteLine("Введите количество чисел цикла");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число начала цикла");
+int Start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число конца цикла");
+int stop = int.Parse(Console.ReadLine()!);
+int[] mass = MassNums(num, Start, stop);
+PrintArray(mass);
+SumNegPos(mass);
