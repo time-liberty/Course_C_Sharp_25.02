27df8d88bf43322d68f53f53e52eb13f979988b8
miracle <q00yuriy@mail.ru>
2023-03-26 19:46:03 +0300

    Task 7_0

---
+++ lesson_7/7_0/Program.cs
@@ -0,0 +1,45 @@
+﻿// задайте двумерны массив размером m*n/
+//заполненный случаными целыми числами
+
+
+
+
+
+void PrintArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+        {
+            arr[i,j] = new Random().Next(from, to);
+        }
+    return arr;
+}
+
+
+
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int Start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = MassNums(num_row, num_column, Start, stop);
+PrintArray(mass);
