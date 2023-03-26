2fc1518bb5588b0ade979905dbfff7c9acc96a34
miracle <q00yuriy@mail.ru>
2023-03-26 14:08:02 +0300

    Task 6_2

---
+++ lesson_6/6_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2. Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+
+
+string convert(int num_1)
+{
+    string res = "";
+    while (num_1 > 0)
+    {
+        res = num_1 % 2 + res;
+        num_1 = num_1 / 2;
+    }
+    return res;
+}
+Console.WriteLine(convert(12));
+
+
+
