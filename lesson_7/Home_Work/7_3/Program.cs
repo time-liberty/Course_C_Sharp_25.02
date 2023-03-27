61bd8329863bffb2e4c71c2f1c4917f9ff6ab1f3
miracle <q00yuriy@mail.ru>
2023-03-27 23:21:32 +0300

    HW Task 7_3

---
+++ lesson_7/Home_Work/7_3/Program.cs
@@ -0,0 +1,62 @@
+﻿ //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+        {
+            arr[i,j] = new Random().Next(from, to);
+        }
+    return arr;
+}
+void poisk(int[,] array,int n )
+{
+    for(int j = 0; j < array.GetLength(0); j++ )
+    {
+        double avarage = 0;
+        for(int i = 0; i < array.GetLength(1); i++)
+        {
+             avarage = (avarage + array[j,i]);
+        }
+        avarage = avarage / n;
+    Console.Write(avarage + "; ");
+    }
+}
+
+
+Console.WriteLine("Введите количество строк");
+int num_row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов");
+int num_column = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите начало цикла");
+int Start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конец массива");
+int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = MassNums(num_row, num_column, Start, stop);
+PrintArray(mass);
+poisk(mass,num_column);
