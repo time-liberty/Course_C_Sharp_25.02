ac3217c08ee8b5fb642c4a25f3ae3af5494bbdbf
miracle <q00yuriy@mail.ru>
2023-03-31 23:00:08 +0300

    HW Task 9_1

---
+++ lesson_9/Home_Work/9_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все чётные
+//натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+void ValuesSet(int N, int M)
+{
+    if (M < N) return;
+    if (M % 2 == 0)
+    Console.Write($" {M} ");
+    ValuesSet(N, M - 1);
+    ValuesSet(N, M);
+    return;
+}
+Console.WriteLine("Введите число начала");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число конца");
+int num_1 = int.Parse(Console.ReadLine()!);
+ValuesSet(num, num_1);
